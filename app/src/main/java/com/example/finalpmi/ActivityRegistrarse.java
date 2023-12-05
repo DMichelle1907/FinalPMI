e9e06ba29938177d29d248d25f95b902cac0a4e9
Bryan Rodriguez <javier.jr068@gmail.com>
2023-12-05 15:16:58 -0600

    subir imagen de camara a firebase subir imagen de galeria a firebase Traer imagen y convertila a base 64

--- app/src/main/java/com/example/finalpmi/ActivityRegistrarse.java
+++ app/src/main/java/com/example/finalpmi/ActivityRegistrarse.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Message;
@@ -37,6 +38,13 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.finalpmi.Data.ResApi;
 import com.example.finalpmi.Data.Users;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,6 +53,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -59,6 +68,13 @@ public class ActivityRegistrarse extends AppCompatActivity {
     Spinner Spinner;
     ImageView Img;
     String currentPhotoPath;
+
+    FirebaseFirestore mfirestore;
+    StorageReference mstorage;
+    Uri selectedImage;
+    //String currentPhotoPath;
+    File foto;
+    String base64Image;
     static final int Peticion_ElegirGaleria = 103;
 
     static final int Peticion_AccesoCamara = 101;
@@ -359,5 +375,168 @@ public class ActivityRegistrarse extends AppCompatActivity {
     }
 
     private void message(String alerta, String correoYaEstaEnUso, ActivityRegistrarse activityRegistrarse) {
+    }
+
+    public void registrar_usuario_respaldo_firebase() {
+        // Respaldo de datos para evitar errores
+        String nombres = edtNombres.getText().toString();
+        String apellidos = edtApellidos.getText().toString();
+        String correo = edtCorreo.getText().toString();
+        String telefono = edtTelefono.getText().toString();
+        String dni = edtDni.getText().toString();
+        String password = edtPassword.getText().toString();
+        // long carrera = Spinner.getSelectedItemId();
+
+        if (nombres.isEmpty() || apellidos.isEmpty() || correo.isEmpty() || telefono.isEmpty() || dni.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Favor, revisar que todos los campos estén llenos", Toast.LENGTH_SHORT).show();
+
+        } else {
+            try {
+                postUser(nombres, apellidos, correo, telefono, dni, password);
+            } catch (Exception e) {
+                e.printStackTrace(); // o maneja la excepción de alguna manera
+                Toast.makeText(getApplicationContext(), "Error al registrar usuario", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void postUser(String nombres, String apellidos, String correo, String telefono, String dni, String password) {
+        // Validación del formato del correo electrónico
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(getApplicationContext(), "Ingrese un correo electrónico válido", Toast.LENGTH_SHORT).show();
+            return; // salir del método si el correo electrónico no es válido
+        }
+
+        // Mapa con datos del usuario
+        Map<String, Object> map = new HashMap<>();
+        map.put("nombre", nombres);
+        map.put("apellido", apellidos);
+        map.put("correo", correo);
+        map.put("telefono", telefono);
+        map.put("dni", dni);
+        map.put("password", password);
+
+        // Agregar usuario a la colección en Firebase
+        mfirestore.collection("usuario")
+                .add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        Toast.makeText(getApplicationContext(), "Registro completo", Toast.LENGTH_LONG).show();
+                        finish();
+                        Intent intent = new Intent(ActivityRegistrarse.this, ActivityReenvio.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Registro no ingresado", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void subirImagenAFirebaseStorage() {
+        StorageReference filePath = mstorage.child("perfiles").child(selectedImage.getLastPathSegment());
+
+        filePath.putFile(selectedImage)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Operación exitosa
+                        Toast.makeText(getApplicationContext(), "Registro completo", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Manejar el error aquí
+                        Toast.makeText(getApplicationContext(), "Error al subir la imagen: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Puedes usar taskSnapshot para obtener información sobre el progreso
+                    }
+                });
+    }
+
+    public void subirImagenAFirebase_galeria() {
+        // Crear una referencia al lugar donde se almacenará la imagen en Firebase Storage
+        StorageReference filePath = mstorage.child("perfiles").child(selectedImage.getLastPathSegment());
+
+        // Subir la imagen
+        filePath.putFile(selectedImage)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // La imagen se subió exitosamente
+                        Toast.makeText(ActivityRegistrarse.this, "Se subió exitosamente la foto.", Toast.LENGTH_SHORT).show();
+                        obtenerImagenBase64();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Manejar el caso en que la subida falle
+                        Toast.makeText(ActivityRegistrarse.this, "Error al subir la foto: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void obtenerImagenBase64() {
+        StorageReference filePath = mstorage.child("perfiles").child("101998");
+
+        filePath.getDownloadUrl().addOnSuccessListener(uri -> {
+            // Obtén la URL de descarga de la imagen
+            String downloadUrl = uri.toString();
+
+            // Inicia una tarea asíncrona para descargar y convertir la imagen a base64
+            new DownloadImageTask().execute(downloadUrl);
+        }).addOnFailureListener(e -> {
+            // Manejar el caso en que la obtención de la URL falle
+            Toast.makeText(ActivityRegistrarse.this, "Error al obtener la URL de la imagen: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String url = urls[0];
+            try {
+                // Descargar la imagen desde la URL
+                InputStream inputStream = new java.net.URL(url).openStream();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                // Convertir la imagen a bytes
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = ((InputStream) inputStream).read(buffer)) != -1) {
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+                // Convertir la imagen a base64
+                return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String base64Image) {
+            // Manejar la cadena de base64 de la imagen
+            if (base64Image != null) {
+                // Hacer algo con la cadena de base64, por ejemplo, mostrarla en un TextView
+                // textView.setText(base64Image);
+                Toast.makeText(ActivityRegistrarse.this, "Imagen en Base64 obtenida exitosamente", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ActivityRegistrarse.this, "Error al obtener la imagen en Base64", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
